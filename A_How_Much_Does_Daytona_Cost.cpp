a9d32b805db705cb3c8da9a333c54c871c691ad9
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-11-10 20:55:52 +0530

    Add files via upload

---
+++ A_How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<climits>
+#include<map>
+using namespace std;
+#define tc() int t;cin>>t;while(t--)
+#define read(type) readInt<type>()
+#define ll long long
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(),(x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+
+        if(find(v.begin(), v.end(), k)!=v.end()){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
