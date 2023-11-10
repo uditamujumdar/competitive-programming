a45cc530c31028a09f82493ed8722a30eba126ea
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-11-10 21:27:55 +0530

    Add files via upload

---
+++ A_Increasing_Sequence.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<climits>
+#include<map>
+using namespace std;
+#define tc() int t;cin>>t;while(t--)
+#define read(type) readInt<type>()
+#define ll long long
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(),(x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll cnt=1;
+        ll b;
+        for(int i=1;i<=n;i++){
+            cin>>b;
+            if(b==cnt){
+                cnt++;
+            }
+            cnt++;
+        }
+        cout<<cnt-1<<endl;
+    }
+}
