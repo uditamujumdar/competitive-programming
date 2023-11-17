73c1f19450f3a816f4347ee2d6f5996576d583aa
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2023-11-17 13:35:38 +0530

    Add files via upload

---
+++ A_Stones_on_the_Table.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<climits>
+#include<map>
+using namespace std;
+#define tc() int t;cin>>t;while(t--)
+#define read(type) readInt<type>()
+#define ll long long
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(),(x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int ans=0;
+
+    for(int i=0;i<n-1;i++){
+        if(s[i]==s[i+1])
+        ans++;
+    }
+
+    cout<<ans<<endl;
+}
