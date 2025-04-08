d33aae63f6764aebadc022afe2bae48861520549
Udita Mujumdar <146844077+uditamujumdar@users.noreply.github.com>
2025-04-08 21:52:26 +0530

    Add files via upload

---
+++ A_Ideal_Generator.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tc() int t;cin>>t;while(t--)
+#define ll long long
+
+int main(){
+    tc(){
+        int k;
+        cin>>k;
+        if(k&1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
